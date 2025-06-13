d670c1f610cab666e60f9a37f579aeb2000c2731
keithagroves <keith.groves@jointheleague.org>
2025-06-13 13:53:44 -0700

    add deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/sourceco/',
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
   }
 })
